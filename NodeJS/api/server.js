353250191f932e6b9c87a6e6ea0fe93554da99c2
Paul FICOT <33148717+PaulFICOT@users.noreply.github.com>
2019-01-22 11:59:53 +0100

    22012019_5P

--- NodeJS/api/server.js
+++ NodeJS/api/server.js
@@ -1,17 +1,97 @@
-//IMPORTS
+//IMPORTS __________________________________________________________________________________________________
 var express = require('express');
+var bodyParser = require('body-parser');
 
-//INSTANTIATE SERVER
+//INSTANTIATE VAR __________________________________________________________________________________________
 var server = express();
+var router_api = express.Router();
 
-//CONFIGURE ROUTES
-server.get('/', function (req, res) {
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).send('<h1>Test server</h1>');
-});
+var hostname = 'localhost';
+var port = 3000;
+
+//CONFIGURE ROUTES _________________________________________________________________________________________
+//GLOBAL STUDENT ROUTE API ______________________________________________________________________
+router_api.route('/students')
+
+//GET, POST, PUT, DELETE HTTP REQUESTS
+//GET
+.get(function(req, res){
+    res.json({
+        message : "Liste des étudiants",
+        methode : req.method
+    });
+})
+
+//POST
+.post(function(req, res){
+    res.json({
+        message : "Ajouter des étudiant",
+        methode : req.method
+    });
+})
+
+//PUT
+.put(function(req, res){
+    res.json({
+        message : "Modifier un étudiant",
+        methode : req.method
+    });
+})
+
+//DELETE
+.delete(function(req, res){
+    res.json({
+        message : "Supprimer un étudiant",
+        methode : req.method
+    });
+})
+
+
+//ROUTE FOR OUR API ______________________________________________________________________
+router_api.route('/students/:student_id')
+
+//GET, PUT, DELETE HTTP REQUESTS
+//GET
+.get(function(req, res){
+    res.json({
+        message : "Vous souhaitez accéder aux informations de l'étudiant n°" + req.params.student_id
+    });
+})
+
+//PUT
+.put(function(req, res){
+    res.json({
+        message : "Vous souhaitez modifier les informations de l'étudiant n°" + req.params.student_id
+    });
+})
+
+//DELETE
+.delete(function(req, res){
+    res.json({
+        message : "Vous souhaitez supprimer les informations de l'étudiant n°" + req.params.student_id
+    });
+})
+
+
+//GLOBAL ROUTE API _______________________________________________________________________
+router_api.route('/')
+
+//ALL HTTP REQUESTS
+//ALL
+.all(function(req, res){
+    res.json({
+        message : "Bienvenue sur l'API du BDE !", methode : req.method
+    });
+})
+
+
+//USE EXPRESS'S MODULES
+server.use(router_api);
+
+server.use(bodyParser.urlencoded({ extended : false}));
+server.use(bodyParser.json());
 
 //LAUNCH SERVER
-server.listen(8080, function() {
-    console.log("server OK");
-    
-})
+server.listen(port, hostname, function() {
+    console.log("Le serveur fonctionne sur : http://" + hostname + ":" + port + "\n");   
+});
