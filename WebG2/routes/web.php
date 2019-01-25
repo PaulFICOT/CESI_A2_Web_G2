baa146df85faf8672ef20de9f0ea16da878156d4
NathanLBC <nathan.lambec@viacesi.fr>
2019-01-25 15:14:31 +0100

    25012019_3N

--- WebG2/routes/web.php
+++ WebG2/routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/inscription', function () {
+Route::get('/inscription', function() {
     return view('inscription');
 });
