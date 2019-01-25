55d3f139c91b7af6a09c7d9c532a9fc0a14f2682
NathanLBC <nathan.lambec@viacesi.fr>
2019-01-25 16:59:02 +0100

    25012019_5N

---
+++ WebG2/resources/views/inscription.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('contenu')
+    <form action="/inscription" method="post">
+        {{  csrf_field() }}
+
+    <input type="email" name="email" placeholder="Email">   
+    <input type="text" name="FirstName" placeholder="Prénom">   
+    <input type="text" name="LastName" placeholder="Nom de famille">   
+    <input type="password" name="password" placeholder="Mot de Passe">   
+    <input type="password" name="password_confirmation" placeholder="Confirmation">   
+    <input type="text" name="Centre" placeholder="Centre">   
+    <input type="submit" value="M'inscrire">   
+
+    </form>
+@endsection
