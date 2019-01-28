ddfaeaf6590d5fe22a926158791e4e42f78ce5fa
NathanLBC <nathan.lambec@viacesi.fr>
2019-01-28 15:49:08 +0100

    28012019_3N

--- WebG2/routes/web.php
+++ WebG2/routes/web.php
@@ -10,24 +10,35 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+ /*Site de base */
 Route::get('/', function () {
     return view('welcome');
 });
-
+    /*Page d'inscription*/
 Route::get('/inscription', function() {
     return view('inscription');
 });
-
+/* Réception du formulaire d'inscription */
 Route::post('/inscription', function () {
-    $users = new App\users;
-    $users->User_mail = request('email');
-    $users->User_firstname = request('FirstName');
-    $users->User_lastname = request('LastName');
-    $users->User_password = bcrypt(request('password')); /*bcryptjs*/
+    /*Validation du formulaire */
+    request()->validate([
+        'email' => ['required', 'email'],
+        'FirstName' => ['required', 'string'],
+        'LastName' => ['required', 'string'],
+        'password' => ['required', 'confirmed', 'min:8'],
+        'password_confirmation' => ['required'],
+        /*'Centre' => [],*/
+    ]);
+        /*On définit les différents champs que l'utilisateur va entrer 
+        pour les envoyer dans la BDD dans l'ordre */
+    $users = App\users::create([
+    'User_mail' => request('email'),
+    'User_firstname' => request('FirstName'),
+    'User_lastname' => request('LastName'),
+    'User_password' => bcrypt(request('password')), /*bcryptjs*/
     /*$users->User_centre = request('Centre');*/
-    $users->save();
-
+    ]);
+        return 'Vous vous êtes bien inscrit';
 });
 
 Route::get('/connexion', function() {
