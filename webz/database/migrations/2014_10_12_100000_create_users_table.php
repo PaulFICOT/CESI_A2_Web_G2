ffd505ecba5cdafb04db7f1d41a0aeb584230233
Paul FICOT <33148717+PaulFICOT@users.noreply.github.com>
2019-01-29 16:46:23 +0100

    29012019_7P

---
+++ webz/database/migrations/2014_10_12_100000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('Id_user');
+            $table->string('User_firstname');
+            $table->string('User_lastname');
+            $table->string('User_email')->unique();
+            $table->string('User_password');
+            $table->string('User_status');
+            $table->string('User_isAdmin');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ webz/resources/views/connexion.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('content')
+    <form action="/connexion" method="post">
+        {{  csrf_field() }}
+
+    <p><input type="email" name="email" placeholder="Email" value="{{ old('email') }}"></p>
+    @if($errors->has('email'))
+        <p>{{ $errors->first('email') }}</p>
+    @endif
+    <p><input type="password" name="password" placeholder="Mot de Passe"></p>  
+    @if($errors->has('password'))
+        <p>{{ $errors->first('password') }}</p>
+    @endif
+    <p><input type="submit" value="Se connecter"></p>   
+
+    </form>
+@endsection
