ff69dc37c28d93a0cc707d41beb25d7971a5bb27
Antoine Desramaut <33145802+AntoineDesramaut@users.noreply.github.com>
2019-01-30 01:02:48 +0100

    30/01/2018/02

--- WebG2Valide/resources/views/Events.blade.php
+++ WebG2Valide/resources/views/Events.blade.php
@@ -13,6 +13,26 @@
   </head>
 
   <body>
+    <nav class="cesiBlue top panel">
+      <div class="sections desktop">
+        <div class="left">
+            <a href="main.html"><img src="ressources/images/logos/logo_cesi_noir.png" alt="logo" class="logo toLeft"></a>
+        </div>
+        <div class="center">
+          <ul class="menu uppercase">
+            <li><a href="retro.html"><p.small>Rétrospective</p.small></a></li>
+            <li><a href="boite_idees.html"><p.small>Boîte à idées</p.small></a></li>
+            <li><a href="boutique.html"><p.small>La Boutique</p.small></a></li>
+          </ul>
+        </div>
+        <div class="right">
+            <a href="login.html"><img src="ressources/images/icones/power.png" alt="connexion" class="icone"></a>
+            <a><img src="ressources/images/icones/panier.png" alt="Panier" class="icone"></a>
+            <a><img src="ressources/images/icones/parametres.png" alt="Paramètres" class="icone"></a>
+        </div>
+      </div>
+    </nav>
+
     <div class="containt">
       <div class="container">
         <div class="wrap">
@@ -23,6 +43,13 @@
         </div>
       </div>
   </div>
+  <footer class="cesiBlue center">
+     <div class="grid">
+       <div class="col-4-12 toCenter"><a href="mentions.html" target="_blank"><p class="tiny">Mentions Légales</p></a></div>
+       <div class="col-4-12 toCenter"><a><p class="tiny">Contactez-nous</p></a></div>
+       <div class="col-4-12 toCenter"><a href="conditions_ventes.html" target="_blank"><p class="tiny">Conditions générales de vente</p></a></div>
+     </div>
+     </footer>
 </body>
 </html>
             @endsection
