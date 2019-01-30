28f9baf0e0d1de6185ef7aa4b44fd5021a36c6a1
Paul FICOT <33148717+PaulFICOT@users.noreply.github.com>
2019-01-30 15:24:33 +0100

    Revert "8p"
    This reverts commit da9cb386307523e2b0b8cd804e080dcfdb42f1c9.

--- WebG2Valide/routes/web.php
+++ WebG2Valide/routes/web.php
@@ -4,24 +4,21 @@
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
+|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+|
 */
-
-    /*Site d' Accueil */
+ /*Site d' Accueil */
 Route::view('/', view('welcome'));
     /*Page d'inscription*/
 Route::get('/inscription', 'InscriptionController@formulaire');
-    /* Réception du formulaire d'inscription */
+/* Réception du formulaire d'inscription */
 Route::post('/inscription', 'InscriptionController@traitement');
 
-    /*Page de connexion*/
 Route::get('/connexion', 'ConnexionController@formulaire');
-    /* Réception du formulaire de connexion */
-Route::post('/connexion', 'ConnexionController@traitement');
 
-    /*Page d'affichage des évènements en AJAX*/
-Route::get('/events', 'EventsController@updateEvents');
+Route::post('/connexion', 'ConnexionController@traitement');
 
 ?>
