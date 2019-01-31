520d52b49f31e013d2659304f05c941acd897504
Antoine Desramaut <33145802+AntoineDesramaut@users.noreply.github.com>
2019-01-31 16:08:53 +0100

    31/01/2019/01

--- WebG2Valide/resources/views/Events.blade.php
+++ WebG2Valide/resources/views/Events.blade.php
@@ -49,7 +49,7 @@
        <div class="col-4-12 toCenter"><a><p class="tiny">Contactez-nous</p></a></div>
        <div class="col-4-12 toCenter"><a href="conditions_ventes.html" target="_blank"><p class="tiny">Conditions générales de vente</p></a></div>
      </div>
-     </footer>
+  </footer>
 </body>
 </html>
             @endsection
--- WebG2Valide/resources/views/connexion.blade.php
+++ WebG2Valide/resources/views/connexion.blade.php
@@ -21,24 +21,24 @@
                           <h3 id="ResponseContact-popUp" class="text-cesiBlue heavy-bottom">Connexion</h3>
                           <div class="label ">
                           {{  csrf_field() }}
-                          
+
                           <label class="text-cesiBlue" for="User_mail">Adresse Email</label>
                         </div>
                           <p><input type="email" name="User_mail" placeholder="Email" value="{{ old('User_mail') }}"></p>
                             @if($errors->has('User_mail'))
                             <p>{{ $errors->first('User_mail') }}</p>
                             @endif
-                          
+
                             <div class="label">
                               <label class="text-cesiBlue" for="password">Mot De Passe</label>
                             </div>
-                            <p><input type="password" name="password" placeholder="Mot de Passe"></p>  
+                            <p><input type="password" name="password" placeholder="Mot de Passe"></p>
                             @if($errors->has('password'))
                             <p>{{ $errors->first('password') }}</p>
                             @endif
                           <p><input class="button cesiBlue round btn margin-top-2 cropBottom" type="submit" value="Se connecter"></p>
                           <p>
-                              <p>Vous ne possèdez pas de compte ?</p>
+                              <p>Vous ne possédez pas de compte ?</p>
                               <a class ="button cesiBlue round btn margin-top-2 cropBottom" href="http://127.0.0.1:8000/inscription">S'inscrire</a>
                           </p>
                         </form>
