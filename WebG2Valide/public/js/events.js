8390f7ced7e8c51fd85a3956d03e23f460c67ab2
Paul FICOT <33148717+PaulFICOT@users.noreply.github.com>
2019-02-01 11:50:48 +0100

    01022019_4P

--- WebG2Valide/public/js/events.js
+++ WebG2Valide/public/js/events.js
@@ -1,15 +1,15 @@
 function yHandler(){
-    var events = document.getElementById('events');
+    /*var events = document.getElementsByClassName('event');
     var contentHeight = events.offsetHeight; //Height of the content
     var yOffset = window.pageYOffset;      //Get the vertical scroll
     var y = yOffset + window.innerHeight;
 
     if(y >= contentHeight) {
         events.innerHTML += '<div class="event">';
-        for(i = 1; i<6; i++){
+        for(i = 1; i<4; i++){
             // Ajax call
             $(function (){
-                var $events = $('#events');
+                var $events = $('#events');*/
                 $.ajax({
                     type: 'GET',
                     url: 'http://127.0.0.1:3000/api/event/' + $id,
@@ -18,9 +18,9 @@ function yHandler(){
                         if($.trim(events)){
                             $.each(events, function(i, event){
                                 $events.append('<h2 id="event"> Nom de l\'événement: ' + event.Event_title + '</h2>');
-                                $events.append('<p id="event">L\'événement aura lieu le x&: ' + event.Event_date + '</p>');
-                                $events.append('<li id="event"> L\'événement au lieu au/à : ' + event.Event_location + '</li>');
-                                $events.append('<p id="event"> Coût de l\'événement: ' + event.Event_price + ' €</p>');
+                                $events.append('<p class="event">L\'événement aura lieu le x&: ' + event.Event_date + '</p>');
+                                $events.append('<li class="event"> L\'événement au lieu au/à : ' + event.Event_location + '</li>');
+                                $events.append('<p class="event"> Coût de l\'événement: ' + event.Event_price + ' €</p>');
                                 $events.append('</div> <div id="slider" class="toCenter heavy-bottom"><figure>');                                $events.append('</figure>');
                                 $events.append('</div>');
                                 $events.append('<div class="toCenter" id="event"> </p>Description de l\'événement: ' + event.Event_description + '</p>');
@@ -36,11 +36,11 @@ function yHandler(){
                         alert("Status: " + textStatus); alert("Error: " + errorThrown);
                     }
                 });
-            });
+}/*);
             $id++;
         }
     }
 }
 window.onload = yHandler;
-window.onscroll = yHandler;
+window.onscroll = yHandler;*/
 $id = 1;
--- WebG2Valide/resources/views/Events.blade.php
+++ WebG2Valide/resources/views/Events.blade.php
@@ -14,6 +14,6 @@
         </div>
       </div>
   </div>
-  <script src="public/js/ajaxevents.js" ></script>
+  <script src="js/events.js" ></script>
   </body>
 @endsection
