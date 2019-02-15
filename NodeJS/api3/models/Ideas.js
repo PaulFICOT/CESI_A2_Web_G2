51b4e2e8ccab6769664123e32cd82edd547a6948
axel-sauvage <33750368+axel-sauvage@users.noreply.github.com>
2019-02-15 09:00:57 +0100

    123

--- NodeJS/api3/models/Ideas.js
+++ NodeJS/api3/models/Ideas.js
@@ -1,18 +1,18 @@
 var db = require('../dbconnection'); //reference of dbconnection.js  
 var Ideas = {  
-    getAllEvents: function(callback) {  
-        return db.query("SELECT * FROM eventz JOIN users ON users.Id_users = eventz.Id_user WHERE eventz.Event_approval=0", callback);  // JOIN users ON users.Id_users = eventz.
+    getAllIdeas: function(callback) {  
+        return db.query("SELECT * FROM eventz WHERE eventz.Event_approval=0", callback);  // JOIN users ON users.Id_users = eventz.
     },  
-    getEventsById: function(id, callback) {  
+    getIdeasById: function(id, callback) {  
         return db.query("SELECT * FROM eventz WHERE eventz.Event_approval = 0 AND eventz.Id_event=?", [id], callback);  // JOIN users ON users.Id_users = eventz.Id_user eventz
     },  
-    addEvent: function(Eventz, callback) {  
+    addIdeas: function(Eventz, callback) {  
         return db.query("INSERT INTO eventz(Event_title, Event_description, Event_approval, Event_date, Event_recurrence, Event_price, Event_period, Event_location, Id_user) VALUES(?,?,?,?,?,?,?,?,?)", [Eventz.Event_title, Eventz.Event_description, Eventz.Event_approval, Eventz.Event_date, Eventz.Event_recurrence, Eventz.Event_price, Eventz.Event_period, Eventz.Event_location, Eventz.Id_user], callback);  
     },  
-    deleteEvent: function(id, callback) {  
+    deleteIdeas: function(id, callback) {  
         return db.query("DELETE FROM eventz WHERE id_event=?", [id], callback);  
     },  
-    updateEvent: function(id, Eventz, callback) {  
+    updateIdeas: function(id, Eventz, callback) {  
         return db.query("UPDATE eventz SET Event_title=?, Event_description=?, Event_approval=?, Event_date=?, Event_recurrence=?, Event_price=?, Event_period=?, Event_location=?, Id_user=? WHERE Id_event=?", [Eventz.Event_title, Eventz.Event_description, Eventz.eventEvent_approval_approval, Eventz.Event_date, Eventz.Event_recurrence, Eventz.Event_price, Eventz.Event_period, Eventz.Event_location, Eventz.Id_user, id], callback); 
     }  
 };  
--- WebG2Valide/routes/web.php
+++ WebG2Valide/routes/web.php
@@ -9,48 +9,41 @@
 | contains the "web" middleware group. Now create something great!
 */
 
- /*Site d' Accueil */
+    /*Vue de l'accueil */
 Route::view('/', 'welcome');
-    /*Page d'inscription*/
+
+    /* Page d'inscription */
 Route::get('/inscription', 'InscriptionController@formulaire');
     /* Réception du formulaire d'inscription */
 Route::post('/inscription', 'InscriptionController@traitement');
 
-    /*Page de connexion*/
+    /* Page de connexion */
 Route::get('/connexion', 'ConnexionController@formulaire');
     /* Réception du formulaire de connexion */
 Route::post('/connexion', 'ConnexionController@traitement');
 
-    /*  */
+    /* Connexion au compte */
 Route::get('/mon-compte', 'CompteController@connecte');
-
-    /*  */
+    /* Deconnexion au compte */
 Route::get('/deconnexion', 'CompteController@deconnexion');
 
-    /*  */
+    /* Vue de la page "Mentions légales" */
 Route::view('/mentions', 'mentions');
 
-    /*  */
+    /* Vue de la page "Conditions Générales d'Utilisation" */
 Route::view('/cgu', 'cgu');
-    /*  */
+
+    /* Vue de la page "Conditions Générales de Ventes" */
 Route::view('/cgv', 'cgv');
 
-    /*  */
+    /* Envoie de l'idée proposée */
 Route::get('/ideabox', 'IdeaboxController@formulaire');
-    /*  */
-Route::post('/ideabox', 'ideaboxController@traitement');
 
-    /*  */
+    /* Traitement des données */
 Route::get('/evenements', 'EventsController@formulaire');
 
-//Route::post('/evenements', 'EventsController@traitement');
-
-    /*  */
+    /* Boutique du BDE */
 Route::get('/boutique', 'BoutiqueController@formulaire');
-    /*  */
+    /* Traitement des données */
 Route::post('/boutique', 'BoutiqueController@traitement');
-
-
-
-Route::view('/test', 'test');
 ?>
